076f9320c93f9273a9f7236c6031fe2a3571a450
Saulo Soares de Toledo <saulotoledo@gmail.com>
2023-02-17 21:33:36 +0000

    feat: setup dotenv for configuration

--- impact_analyzer/settings.py
+++ impact_analyzer/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dotenv
+import os
 from pathlib import Path
+
+# Load variables from .env
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k#!xr!7+3si0jr5&-^f-v=09+-*_d-xkj4f$u63h947z3e*to2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 

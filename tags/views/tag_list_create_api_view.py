ceecd7e712a7ef7c0486602c73dd159c1678daac
Saulo Soares de Toledo <saulotoledo@gmail.com>
2023-02-25 18:15:07 +0000

    fix(tags): allow using swagger UI to set request body

--- tags/views/tag_list_create_api_view.py
+++ tags/views/tag_list_create_api_view.py
@@ -1,13 +1,24 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from tags.serializers.tag_serializer import TagSerializer
 
 
 class TagListCreateAPIView(APIView):
-    allowed_methods = ['get', 'post']
+    http_method_names = ['get', 'post']
     service = None
     serializer_class = None
 
+    @swagger_auto_schema(
+        responses={
+            200: 'Success',
+            400: 'Bad Request'
+        },
+        operation_summary='Retrieves all tags',
+        operation_description='Retrieves all tags',
+    )
     def get(self, request):
         """Returns all tags in the database"""
 
@@ -15,6 +26,17 @@ class TagListCreateAPIView(APIView):
         serializer = self.serializer_class(tags, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        method='post',
+        request_body=TagSerializer,
+        responses={
+            201: 'Created',
+            400: 'Bad Request'
+        },
+        operation_summary='Creates a tag',
+        operation_description='Creates a new tag',
+    )
+    @action(detail=True, methods=['post']) # Required by @swagger_auto_schema to generate the request body
     def post(self, request):
         """Creates a new tag"""
 
--- tags/views/tag_retrieve_update_destroy_api_view.py
+++ tags/views/tag_retrieve_update_destroy_api_view.py
@@ -1,13 +1,24 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from tags.serializers import TagSerializer
 
 
 class TagRetrieveUpdateDestroyAPIView(APIView):
-    allowed_methods = ['get', 'put', 'delete']
+    http_method_names = ['get', 'put', 'delete']
     service = None
     serializer_class = None
 
+    @swagger_auto_schema(
+        responses={
+            200: 'Success',
+            400: 'Bad Request'
+        },
+        operation_summary='Retrieves a tag',
+        operation_description='Retrieves a tag by ID',
+    )
     def get(self, request, id=None):
         """Returns a single tag"""
 
@@ -18,6 +29,17 @@ class TagRetrieveUpdateDestroyAPIView(APIView):
         serializer = self.serializer_class(tag)
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        method='put',
+        request_body=TagSerializer,
+        responses={
+            200: 'Success',
+            400: 'Bad Request'
+        },
+        operation_summary='Updates a tag',
+        operation_description='Updates a tag by ID',
+    )
+    @action(detail=True, methods=['put']) # Required by @swagger_auto_schema to generate the request body
     def put(self, request, id):
         """Updates a single tag"""
 
@@ -29,6 +51,14 @@ class TagRetrieveUpdateDestroyAPIView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(
+        responses={
+            204: 'No Content',
+            400: 'Bad Request'
+        },
+        operation_summary='Deletes a tag',
+        operation_description='Deletes a tag by ID',
+    )
     def delete(self, request, id):
         """Deletes a single tag"""
 
